eeecf3adde10bad803a528753f331c215567df09
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2023-12-20 00:57:05 +0530

    Update custom apache.sh

--- custom apache.sh
+++ custom apache.sh
@@ -7,9 +7,4 @@ ls
 sudo build -t abc .
 # Run a container in the background, mapping port 6000 on the host to port 80 in the container
 sudo run -d -p 6000:80 abc
-# Tag the image
-docker tag abc abhisheksharma9877/apache-image:abcd
-# Login to Docker Hub
-docker login -u $duser -p $dpassword
-# Push the image to Docker Hub
-docker push abhisheksharma9877/apache-image:abcd
+echo "done"
