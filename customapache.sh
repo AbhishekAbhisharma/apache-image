0ab0247af1b99b5adbedabc82fbe02f8b655851c
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2023-12-20 01:54:53 +0530

    Update customapache.sh

--- customapache.sh
+++ customapache.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 ls
+# Specify the port number
+port_to_check=6000
+# Find the container ID of the running container on the specified port
+container_id=$(docker ps -a --filter "publish=${port_to_check}:80" --format "{{.ID}}")
+if [ -z "$container_id" ]; then
+  echo "No container found running on port $port_to_check."
+  exit 1
+fi
+# Stop the running container
+docker stop "$container_id"
+
+# Remove the stopped container
+docker rm "$container_id"
+echo "Container on port $port_to_check stopped and removed."
+
 # Build the Docker image
 sudo docker build -t abc .
 # Run a container in the background, mapping port 6000 on the host to port 80 in the container
