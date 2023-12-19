c7acd3ae444da0126c7b6eb6f357c72d4e8dae0a
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2023-12-20 01:43:25 +0530

    Update customapache.sh

--- customapache.sh
+++ customapache.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ls
-#Navigate to the directory containing your Dockerfile and application code
-ls
 # Build the Docker image
 sudo build -t abc .
 # Run a container in the background, mapping port 6000 on the host to port 80 in the container
