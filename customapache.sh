53a837a973b756e0ed130cb5ad5b91453cec7739
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2023-12-20 16:00:15 +0530

    Update customapache.sh

--- customapache.sh
+++ customapache.sh
@@ -18,5 +18,5 @@ echo "Container on port $port_to_check stopped and removed."
 # Build the Docker image
 sudo docker build -t abc .
 # Run a container in the background, mapping port 6000 on the host to port 80 in the container
-sudo docker run -d -p 6000:80 abc
+sudo docker run -d -p 9000:80 abc
 echo "done"
